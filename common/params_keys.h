de50aa018dff2b088ae46dde60b764ea193cfb8e
TiphTech <tiphaine_c@icloud.com>
2026-07-07 19:00:58 +0200

    Default variable torque factor to enabled

--- common/params_keys.h
+++ common/params_keys.h
@@ -307,7 +307,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
 
     {"LateralTorqueCustom", {PERSISTENT, INT, "1"}},
     {"LateralTorqueAccelFactor", {PERSISTENT, INT, "2500"}},
-    {"TorqueAccelFactorVariable", {PERSISTENT, INT, "0"}},
+    {"TorqueAccelFactorVariable", {PERSISTENT, INT, "1"}},
     {"LateralTorqueFriction", {PERSISTENT, INT, "100"}},
     {"LateralTorqueKpV", {PERSISTENT, INT, "100"}},
     {"LateralTorqueKiV", {PERSISTENT, INT, "10"}},
